05022475711208600b0770862bc1d3533453ba46
Alex Tucker <tucker.ale@gmail.com>
2023-07-25 12:00:42 +0700

    final

---
+++ js/directory.js
@@ -0,0 +1,62 @@
+// Solution after getting a hint from the solution video
+const main = document.querySelector('main');
+let html = '';
+
+for (let i = 0; i < pets.length; i++) {
+    let pet = pets[i];
+    html += `
+        <h2>${pet.name}</h2>
+        <h3>${pet.type} | ${pet.breed}</h3>
+        <p>Age: ${pet.age}</p>
+        <img src="img/${pet.photo}" alt="${pet.breed}">
+    `;
+}
+
+main.insertAdjacentHTML('beforeend', html);
+
+// document.querySelector('main').innerHTML = html;
+
+/* ====== MY ATTEMPT ======
+
+let petName = [];
+let petType = [];
+let petBreed = '';
+let petAge = '';
+let petPhoto = '';
+
+
+for (let i = 0; i < pets.length; i++) {
+  let name = pets[i].name;
+  let type = pets[i].type;
+  if (true) {
+    
+  }
+}
+
+
+function createListItems(arr) {
+  let items = '';
+  for (let i = 0; i < arr.length; i++) {
+      items += `<li> ${arr[i]}</li>`;
+  }
+  return items;
+}
+
+let html = `
+${createListItems(petName)}
+${createListItems(petType)}
+
+`;
+
+document.querySelector('main').innerHTML = html;
+
+
+
+for (let petProp in pets) {
+  console.log(pets[petProp]);
+}
+
+console.log(`${}`)
+
+console.log(pets[0].name);
+*/
---
+++ js/pets.js
@@ -0,0 +1,51 @@
+/*
+  Create an array of 'pet' objects.
+  Each object (6) should have the following properties (5): 
+  name, type, breed, age, and photo
+*/
+
+const pets = [
+  {
+    name: 'Blue',
+    type: 'Dog',
+    breed: 'Australian Shepherd',
+    age: 3,
+    photo: 'aussie.jpg'
+  },
+  {
+    name: 'Dilbert',
+    type: 'Dog',
+    breed: 'Dachshund',
+    age: 2,
+    photo: 'dachshund.jpg'
+  },
+  {
+    name: 'Gorky',
+    type: 'Dog',
+    breed: 'Golden',
+    age: 4,
+    photo: 'golden.jpg'
+  },
+  {
+    name: 'Ginger',
+    type: 'Cat',
+    breed: 'Persian Cat',
+    age: 6,
+    photo: 'persian.jpg'
+  },
+  {
+    name: 'Pickles',
+    type: 'Dog',
+    breed: 'Pug',
+    age: 2,
+    photo: 'pug.jpg'
+  },
+  {
+    name: 'Tabbers',
+    type: 'Cat',
+    breed: 'Tabby Cat',
+    age: 7,
+    photo: 'tabby.jpg'
+  }
+];
+
